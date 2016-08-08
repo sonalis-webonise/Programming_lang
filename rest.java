9ba79a502bc1e3f8bb9fe1f66f168c023aea63a2
sonalis-webonise <sssonali63@gmail.com>
2016-08-08 15:09:28 +0530

    Add files via upload
    Updated File

--- rest.java
+++ rest.java
@@ -7,9 +7,12 @@ public class rest
 	{
 		int id[];
 		String price;
-		String item;
+		String item1;
+		String item2;
+		String[] item;
 		String line = null;
-		BufferedReader br=new BufferedReader(new FileReader("sample_data.csv"));
+		rest r=new rest();
+		BufferedReader br=new BufferedReader(new FileReader("C:/Users/Sonali/workspace/Rest/src/Sample_data.csv"));
 		List<String> lines = new ArrayList<>();
 		int i=0;
 		
@@ -19,35 +22,84 @@ public class rest
 			System.out.println(lines.get(i));
 			i++;
 		}
-
+		item=new String[2];
 
 
 		System.out.println("Enter items that you want");
 		BufferedReader read=new BufferedReader(new InputStreamReader(System.in));		
-		item=read.readLine();
-List<String> temp = new ArrayList<>();
-				
+		
+		for(i=0;i<2;i++)
+		{
+			item[i]=read.readLine();
+		}
+		List<String> tid = new ArrayList<>();
+		List<String> tprice = new ArrayList<>();
+		List<String> titem = new ArrayList<>();
+		List<Integer> index = new ArrayList<>();
+		List<Integer> idstack = new ArrayList<>();
+		
+		
 		for(i=0;i<lines.size();i++)
 		{
-		if(lines.get(i).split(",")[2].contains(item))
-		{	
-		System.out.println(temp.add(lines.get(i).split(",")[0]));
-		System.out.println(temp.add(lines.get(i).split(",")[1]));		
-		System.out.println(lines.get(i).split(",")[2].contains(item));
-			temp.add(item);
-		if(temp.get(i)==temp.get(i+1))
-		{
-			System.out.println("Hotel id " + i);
+		tid.add(lines.get(i).split(",")[0]);
+		tprice.add(lines.get(i).split(",")[1]);		
+		titem.add(lines.get(i).split(",")[2]);
 		}
-		}
-		}
-
-		for(i=0;i<temp.size();i++)
-		{
-		System.out.println(temp.get(i));
-		}
-
+		//System.out.print(titem.size());
 		
+		for(i=0;i<titem.size();i++)
+		{
+			for(int j=0;j<item.length;j++)
+			{
+				if(titem.get(i).contains(item[j]))
+				{	
+					index.add(i);
+					
+					idstack.add(Integer.parseInt(tid.get(i)));
+					System.out.print(" Avail .... " + idstack.get(i));
+					//System.out.print("  .... " + i);
+					
+					
+				}
+			}
+		}
+		int sum=0;
+		int numdup=0,dupcnt=0;
+		int prev=-1;
+		for(i=0;i<idstack.size();i++)
+		{
+			if(idstack.get(i)==prev)
+			{
+				++numdup;
+				if(numdup==1)
+				{
+					++dupcnt;
+					if(dupcnt==1)
+					{
+						System.out.println("val   " + idstack.get(i));
+					}
+					else
+					{
+						System.out.println(", " + idstack.get(i));
+						
+					}
+					
+				}
+			}
+			else
+			{
+				prev=idstack.get(i);
+				numdup=0;
+			}
+			
+			System.out.println(" \n " + tprice.get(i));
+			int t=Integer.parseInt(tprice.get(i));
+			sum=sum+t;
+			System.out.println("\nPrice\nsum ::" + sum);
+		}
 
+		System.out.println(" No: " + dupcnt);
+		
+	
 	}
 }
